20cde2863f89fc2524ed48c8ba392590a6acf3d9
Anaelise Huaniri Romero <aanesita@gmail.com>
2025-10-16 10:37:18 -0400

    Divisibilidad, version otra vez  con Python

---
+++ 1DivisvilidadPy/Divisibilidad.py
@@ -0,0 +1,16 @@
+def main():
+    while True:
+        try:
+            a,b = map(int,input().split())
+            if a % b == 0:
+                print(f"{a} es divisible por {b}")
+            elif b%a == 0:
+                print(f"{b} es divisible por {a}")
+            else:
+                print(-1)
+        except EOFError:
+            break
+        
+if __name__ == "__main__":
+    main()
+        
